4dc39e9e241536cba57f7c4a688855b00c04e4e2
lopezdylan <lopezdylanca@gmail.com>
2022-12-15 12:58:54 -0500

    Added design and styling to register page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Login = () => {
+  return (
+    <div className='auth'>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder='username' />
+        <input type="password" placeholder='password' />
+        <button>Login</button>
+        <p>Invalid username or password</p>
+        <span><Link to="/register">Want to sign up?</Link></span>
+      </form>
+    </div>
+  )
+}
+
+export default Login
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Register = () => {
+  return (
+    <div className='auth'>
+      <h1>Register</h1>
+      <form>
+        <input required type="text" placeholder='username' />
+        <input required type="email" placeholder='email' />
+        <input required type="password" placeholder='password' />
+        <button>Login</button>
+        <p>Invalid username or password</p>
+        <span><Link to="/login">Do you have an account?</Link></span>
+      </form>
+    </div>
+  )
+}
+
+export default Register
