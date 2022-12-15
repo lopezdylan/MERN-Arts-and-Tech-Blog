0fe987f0db1ad3e46ce7bfaf12861477198fc8b1
lopezdylan <lopezdylanca@gmail.com>
2022-12-15 18:08:47 -0500

    Added single post design and initial styling

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Edit from '../assets/img/edit.png'
+import Delete from '../assets/img/delete.png'
+import { Link } from 'react-router-dom'
+
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className="single">
+      {/* CONTENT SECTION */}
+      <div className="content">
+        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        
+        <div className="user">
+          <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <div className="info">
+            <span>Username</span>
+            <p>Post x days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!</p>
+      </div>
+      {/* MENU BAR */}
+      <div className="menu">
+        MMMM
+      </div>
+    </div>
   )
 }
 
