910ee42d724988ed14f405f74036b6f9e399f328
lopezdylan <lopezdylanca@gmail.com>
2022-12-15 14:20:53 -0500

    Added navbar design and initial sytling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Logo from "../assets/img/ld-blog-logo.png"
+import  {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className="container">
+            <div className="logo">
+                <img src={Logo} alt="" />
+            </div>
+            <div className="links">
+                <Link className='link' to="/?cat=art">
+                    <h6>ART</h6>
+                </Link>
+                <Link className='link' to="/?cat=art">
+                    <h6>SCIENCE</h6>
+                </Link>
+                <Link className='link' to="/?cat=art">
+                    <h6>TECHNOLOGY</h6>
+                </Link>
+                <Link className='link' to="/?cat=art">
+                    <h6>CINEMA</h6>
+                </Link>
+                <Link className='link' to="/?cat=art">
+                    <h6>DESGIN</h6>
+                </Link>
+                <Link className='link' to="/?cat=art">
+                    <h6>FOOD</h6>
+                </Link>
+                <span>username</span>
+                <span>Logout</span>
+                <span className='write'>
+                    <Link className='link' to="/write">Write</Link>
+                </span>
+            </div>
+        </div>
+    </div>
   )
 }
 
