fc4601572301d5f9027ae2a2995feeeea7c02b89
lopezdylan <lopezdylanca@gmail.com>
2022-12-15 19:43:19 -0500

    Added design for write post page and initial styling

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+// Importing react-quill
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
+  const [value, setValue] = useState('');
+
+  console.log(value);
   return (
-    <div>Write</div>
+    <div className='write-post'>
+      <div className="content">
+        <input type="text" placeholder='Title' />
+        <div className="editor-container">
+          <ReactQuill className='editor' theme="snow" value={value} onChange={setValue} />
+        </div>
+      </div>
+      <div className="menu">
+        {/* Publish */}
+        <div className="item">
+          <h2>Publish</h2>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility </b> Public
+          </span>
+          <input type="file"  name="" id="file" style={{display:"none"}}/>
+          <label className='file' htmlFor="file">Upload Image</label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        {/* Category */}
+        <div className="item">
+          <h2>Category</h2>
+          <div className="category">
+            <input type="radio" name='category' value="art" />
+            <label htmlFor="art">Art</label>
+          </div>
+          <div className="category">
+            <input type="radio" name='category' value="science" />
+            <label htmlFor="art">Science</label>
+          </div>
+          <div className="category">
+            <input type="radio" name='category' value="technology" />
+            <label htmlFor="art">Technology</label>
+          </div>
+          <div className="category">
+            <input type="radio" name='category' value="cinema" />
+            <label htmlFor="art">Cinema</label>
+          </div>
+          <div className="category">
+            <input type="radio" name='category' value="Design" />
+            <label htmlFor="art">Design</label>
+          </div>
+          <div className="category">
+            <input type="radio" name='category' value="food" />
+            <label htmlFor="art">Food</label>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
